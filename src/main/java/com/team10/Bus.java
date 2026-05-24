fb8e7f68ea92d0bb563e6b6627c1d84b88c3a0b8
Jay Moxham <S4169495@student.rmit.edu.au>
2026-05-24 21:27:05 +1000

    getters and contructors in bus.java

--- src/main/java/com/team10/Bus.java
+++ src/main/java/com/team10/Bus.java
@@ -1,7 +1,30 @@
 package com.team10;
+
+
 public class Bus {
     private String busID;
     private int capacity;
     private double fuelLevel;
     private String fuelType; // Diesel, Hybrid, Electricity
+
+    public Bus(String busID, int capacity, double fuelLevel, String fuelType) {
+        this.busID = busID;
+        this.capacity = capacity;
+        this.fuelLevel = fuelLevel;
+        this.fuelType = fuelType;
+    }
+
+    public String getBusID(){
+        return busID;
+    }
+    public int getCapacity(){
+        return capacity;
+    }
+    public double getFuelLevel(){
+        return fuelLevel;
+    }
+    public String getFuelType(){
+        return fuelType;
+    }
+
 }
